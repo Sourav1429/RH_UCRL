236c838391477c3be1e7ac308a7d6a302b1483a1
Sourav1429 <68081019+Sourav1429@users.noreply.github.com>
2026-03-19 17:18:41 -0400

    Changed gym to gymnasium

--- env.py
+++ env.py
@@ -4,14 +4,15 @@ Created on Thu Mar 19 13:30:29 2026
 
 @author: Sourav
 """
-
-import gym
+import gymnasium as gym
 import numpy as np
 
 class AdversarialPendulum:
     def __init__(self, delta_max=0.1):
         self.env = gym.make("Pendulum-v1")
         self.delta_max = delta_max
+        self.observation_space = self.env.observation_space
+        self.action_space = self.env.action_space
 
     def reset(self):
         s, _ = self.env.reset()
